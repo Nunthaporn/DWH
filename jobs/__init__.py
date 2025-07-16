6fc0c50cc53ba06afec0e427493fcc8dcc1d7d20
Nunthaporn Pathi <nunthaporn.pat@fin.com>
2025-07-16 12:25:20 +0700

    update

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -2,9 +2,11 @@ from dagster import Definitions, define_asset_job, asset
 from .dim_company import dim_company_etl
 from .dim_sales import dim_sales_etl
 from .dim_agent import dim_agent_etl
+from .dim_car import dim_car_etl
 from schedules.schedules import dim_company_schedule  # ✅ เพิ่ม import schedule
 from schedules.schedules import dim_sales_schedule  # ✅ เพิ่ม import schedule
 from schedules.schedules import dim_agent_schedule  # ✅ เพิ่ม import schedule
+from schedules.schedules import dim_car_schedule
 
 # ✅ ของเก่า (asset)
 @asset
@@ -17,6 +19,6 @@ assets = [my_simple_asset]
 # ✅ รวม definitions ทั้งหมด
 defs = Definitions(
     assets=assets,
-    jobs=[dim_company_etl, dim_sales_etl,dim_agent_etl], 
-    schedules=[dim_company_schedule,dim_sales_schedule,dim_agent_schedule ],                         # schedule ที่มี
+    jobs=[dim_company_etl, dim_sales_etl,dim_agent_etl,dim_car_etl], 
+    schedules=[dim_company_schedule,dim_sales_schedule,dim_agent_schedule, dim_car_schedule ],                         # schedule ที่มี
 )
