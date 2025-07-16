627736540d7afaef19763de6d2419027a97d84ac
Nunthaporn Pathi <nunthaporn.pat@fin.com>
2025-07-16 12:25:20 +0700

    update

--- schedules/schedules.py
+++ schedules/schedules.py
@@ -2,7 +2,7 @@ from dagster import ScheduleDefinition
 from jobs.dim_company import dim_company_etl
 from jobs.dim_sales import dim_sales_etl
 from jobs.dim_agent import dim_agent_etl
-from jobs.dim_agent import dim_car_etl
+from jobs.dim_car import dim_car_etl
 
 
 dim_company_schedule = ScheduleDefinition(
