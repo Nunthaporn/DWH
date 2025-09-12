4cacc0834ebf1e5281b667b1c9794aa2521fce44
Nunthaporn Pathi <nunthaporn.pat@fin.com>
2025-09-12 11:00:41 +0700

    edit code

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,11 +1,8 @@
 from dagster import Definitions, define_asset_job, asset
 from .dim_company import dim_company_etl
-from .dim_sales import dim_sales_etl
 from .dim_car import dim_car_etl
 from .dim_customer import dim_customer_etl
 from .dim_agent import dim_agent_etl
-from .dim_order_type import dim_order_type_etl
-from .dim_payment_plan import dim_payment_plan_etl
 from .fact_check_price import fact_check_price_etl
 from .fact_installment_payments import fact_installment_payments_etl
 from .fact_sales_quotation import fact_sales_quotation_etl
@@ -17,7 +14,6 @@ from .update_car_id import update_fact_sales_quotation_car_id
 from .update_customer_id import update_fact_sales_quotation_customer_id
 from .update_payment_plan_id import update_fact_sales_quotation_payment_plan_id
 from .update_order_type_id import update_fact_sales_quotation_order_type_id
-from .update_sales_id import update_fact_sales_quotation_sales_id
 from schedules.schedules import dim_company_schedule  
 from schedules.schedules import dim_sales_schedule  
 from schedules.schedules import dim_agent_schedule  
@@ -47,11 +43,11 @@ assets = [my_simple_asset]
 
 defs = Definitions(
     assets=assets,
-    jobs=[dim_company_etl, dim_sales_etl,dim_agent_etl,dim_car_etl,fact_check_price_etl,
-          dim_customer_etl,dim_order_type_etl,dim_payment_plan_etl,fact_installment_payments_etl,
+    jobs=[dim_company_etl,dim_agent_etl,dim_car_etl,fact_check_price_etl,
+          dim_customer_etl,fact_installment_payments_etl,
           fact_sales_quotation_etl,fact_commission_etl,dim_card_agent_etl,fact_insurance_motor_etl,
           update_fact_sales_quotation_agent_id,update_fact_sales_quotation_car_id,update_fact_sales_quotation_customer_id,
-          update_fact_sales_quotation_payment_plan_id,update_fact_sales_quotation_order_type_id,update_fact_sales_quotation_sales_id], 
+          update_fact_sales_quotation_payment_plan_id,update_fact_sales_quotation_order_type_id,], 
     schedules=[dim_company_schedule,dim_sales_schedule,dim_agent_schedule, dim_car_schedule,
                fact_check_price_schedule,dim_customer_schedule,dim_order_type_schedule,
                dim_payment_plan_schedule,fact_installment_payments_schedule,
--- schedules/schedules.py
+++ schedules/schedules.py
@@ -1,11 +1,8 @@
 from dagster import ScheduleDefinition
 from jobs.dim_company import dim_company_etl
-from jobs.dim_sales import dim_sales_etl
 from jobs.dim_car import dim_car_etl
 from jobs.dim_customer import dim_customer_etl
 from jobs.dim_agent import dim_agent_etl
-from jobs.dim_order_type import dim_order_type_etl
-from jobs.dim_payment_plan import dim_payment_plan_etl
 from jobs.fact_check_price import fact_check_price_etl
 from jobs.fact_installment_payments import fact_installment_payments_etl
 from jobs.fact_sales_quotation import fact_sales_quotation_etl
@@ -17,14 +14,9 @@ from jobs.update_car_id import update_fact_sales_quotation_car_id
 from jobs.update_customer_id import update_fact_sales_quotation_customer_id
 from jobs.update_payment_plan_id import update_fact_sales_quotation_payment_plan_id
 from jobs.update_order_type_id import update_fact_sales_quotation_order_type_id
-from jobs.update_sales_id import update_fact_sales_quotation_sales_id
 
 dim_company_schedule = ScheduleDefinition(
     job=dim_company_etl,
-    cron_schedule="0 * * * *", 
-)
-dim_sales_schedule = ScheduleDefinition(
-    job=dim_sales_etl,
     cron_schedule="0 * * * *", 
 )
 dim_agent_schedule = ScheduleDefinition(
@@ -37,14 +29,6 @@ dim_car_schedule = ScheduleDefinition(
 )
 dim_customer_schedule = ScheduleDefinition(
     job=dim_customer_etl,
-    cron_schedule="0 * * * *", 
-)
-dim_order_type_schedule = ScheduleDefinition(
-    job=dim_order_type_etl,
-    cron_schedule="0 * * * *", 
-)
-dim_payment_plan_schedule = ScheduleDefinition(
-    job=dim_payment_plan_etl,
     cron_schedule="0 * * * *", 
 )
 fact_check_price_schedule = ScheduleDefinition(
@@ -89,9 +73,5 @@ update_payment_plan_id_schedule = ScheduleDefinition(
 )
 update_order_type_id_schedule = ScheduleDefinition(
     job=update_fact_sales_quotation_order_type_id,
-    cron_schedule="0 * * * *", 
-)
-update_sales_id_schedule = ScheduleDefinition(
-    job=update_fact_sales_quotation_sales_id,
     cron_schedule="0 * * * *", 
 )
