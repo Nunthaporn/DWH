dac5b6f7d839247a003dc645df5687c2dedece98
Nunthaporn Pathi <nunthaporn.pat@fin.com>
2025-09-15 12:34:41 +0700

    edit schedules

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -17,7 +17,7 @@ from .update_order_type_id import update_order_type_id_on_fact
 from schedules.schedules import dim_company_schedule  
 from schedules.schedules import dim_agent_schedule  
 from schedules.schedules import dim_car_schedule
-from schedules.schedules import dim_customer_schedule
+# from schedules.schedules import dim_customer_schedule
 from schedules.schedules import fact_check_price_schedule
 from schedules.schedules import fact_installment_payments_schedule
 from schedules.schedules import fact_sales_quotation_schedule
@@ -26,7 +26,7 @@ from schedules.schedules import dim_card_agent_schedule
 from schedules.schedules import fact_insurance_motor_schedule
 from schedules.schedules import update_agent_id_schedule
 from schedules.schedules import update_car_id_schedule
-from schedules.schedules import update_customer_id_schedule
+from schedules.schedules import update_company_id_schedule
 from schedules.schedules import update_payment_plan_id_schedule
 from schedules.schedules import update_order_type_id_schedule
 
@@ -40,14 +40,14 @@ assets = [my_simple_asset]
 defs = Definitions(
     assets=assets,
     jobs=[dim_company_etl,dim_agent_etl,dim_car_etl,fact_check_price_etl,
-          dim_customer_etl,fact_installment_payments_etl,
-          fact_sales_quotation_etl,fact_commission_etl,dim_card_agent_etl,fact_insurance_motor_etl,
+          fact_installment_payments_etl, fact_sales_quotation_etl,
+          fact_commission_etl,dim_card_agent_etl,fact_insurance_motor_etl,
           update_agent_id_on_fact,update_car_id_on_fact,update_company_id_job,
           update_payment_plan_id_on_fact,update_order_type_id_on_fact,], 
     schedules=[dim_company_schedule,dim_agent_schedule, dim_car_schedule,
-               fact_check_price_schedule,dim_customer_schedule,fact_installment_payments_schedule,
+               fact_check_price_schedule,fact_installment_payments_schedule,
                fact_sales_quotation_schedule, fact_commission_schedule,dim_card_agent_schedule,
-               fact_insurance_motor_schedule,update_agent_id_schedule,update_car_id_schedule,update_customer_id_schedule,
+               fact_insurance_motor_schedule,update_agent_id_schedule,update_car_id_schedule,update_company_id_schedule,
                update_payment_plan_id_schedule,update_order_type_id_schedule],              
 )
 
--- schedules/schedules.py
+++ schedules/schedules.py
@@ -1,7 +1,7 @@
 from dagster import ScheduleDefinition
 from jobs.dim_company import dim_company_etl
 from jobs.dim_car import dim_car_etl
-from jobs.dim_customer import dim_customer_etl
+# from jobs.dim_customer import dim_customer_etl
 from jobs.dim_agent import dim_agent_etl
 from jobs.fact_check_price import fact_check_price_etl
 from jobs.fact_installment_payments import fact_installment_payments_etl
@@ -27,10 +27,10 @@ dim_car_schedule = ScheduleDefinition(
     job=dim_car_etl,
     cron_schedule="*/15 * * * *", 
 )
-dim_customer_schedule = ScheduleDefinition(
-    job=dim_customer_etl,
-    cron_schedule="*/15 * * * *", 
-)
+# dim_customer_schedule = ScheduleDefinition(
+#     job=dim_customer_etl,
+#     cron_schedule="*/15 * * * *", 
+# )
 fact_check_price_schedule = ScheduleDefinition(
     job=fact_check_price_etl,
     cron_schedule="*/15 * * * *", 
@@ -63,7 +63,7 @@ update_car_id_schedule = ScheduleDefinition(
     job=update_car_id_on_fact,
     cron_schedule="1/15 * * * *", 
 )
-update_customer_id_schedule = ScheduleDefinition(
+update_company_id_schedule = ScheduleDefinition(
     job=update_company_id_job,
     cron_schedule="1/15 * * * *", 
 )
